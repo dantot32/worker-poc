730f46279c9b94e3e8258ccbab48e843c58a4b8e
Daniele Totisco <d.totisco@cdshotels.it>
2025-06-28 17:17:09 +0200

    feat: working example of worker installed as windows service

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
+using Microsoft.Extensions.Options;
 using Serilog;
+using Serilog.Events;
 using System.Runtime.InteropServices;
-using Microsoft.Extensions.Hosting;
 using WorkerPoc;
 
 var builder = Host.CreateApplicationBuilder(args);
@@ -8,18 +9,30 @@ var builder = Host.CreateApplicationBuilder(args);
 // since will be run as windows service
 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
     builder.Services.Configure<HostOptions>(opt => opt.ShutdownTimeout = TimeSpan.FromSeconds(30));
+    builder.Services.AddWindowsService(opt => opt.ServiceName = "WorkerPocService");
 
 // serilog config
 var logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "WorkerPocService", "logs");
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.File(Path.Combine(logPath, "worker-poc-.log"), rollingInterval: RollingInterval.Day)
-    .ReadFrom.Configuration(builder.Configuration)
-    .Enrich.FromLogContext()
-    .CreateLogger();
 
-//#if RELEASE
-//    builder.Logging.ClearProviders(); // otherwise console log wont be written
-//#endif
+var logConfig = new LoggerConfiguration()
+            .WriteTo.File(Path.Combine(logPath, "worker-poc-.log"), rollingInterval: RollingInterval.Day)
+            .ReadFrom.Configuration(builder.Configuration)
+            .Enrich.FromLogContext();
+
+#if RELEASE
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    logConfig.WriteTo.EventLog(source: "WorkerPocService Source",
+                                logName: "Application",
+                                manageEventSource: true,
+                                restrictedToMinimumLevel: LogEventLevel.Information);
+#endif
+
+Log.Logger = logConfig.CreateLogger();
+
+#if RELEASE
+    // this is needed since we wonna get rid of default logging providers such as EventLog configured in 'AddWindowsService'
+    builder.Logging.ClearProviders(); // otherwise console log wont be written
+#endif
 builder.Logging.AddSerilog();
 
 // register services to host
